737ddb9e98a1e204cda2b0858d1ace7dd338a6d4
Thomas Medina <thomasmedina397@gmail.com>
2020-05-18 21:23:33 -0500

    Se agrego un nuevo atributo y se creo el metodo costo_variable

--- Clases.py
+++ Clases.py
@@ -1,8 +1,9 @@
 class Costos:
-    def __init__ (self, nivel, costo_total, precio):
+    def __init__ (self, nivel, costo_total, precio, costo_fijo):
         self.__nivel = nivel
         self.__costo_total = costo_total
         self.__precio = precio
+        self.__costo_fijo = costo_fijo
     
     @property
     def nivel(self):
@@ -16,6 +17,10 @@ class Costos:
     def precio(self):
         return self.__precio
 
+    @property
+    def costo_fijo(self):
+        return self.__costo_fijo
+
     @nivel.setter
     def nivel(self,valor1):
         self.__nivel = valor1
@@ -28,4 +33,9 @@ class Costos:
     def precio(self,valor1):
         self.__precio = valor1
 
-    
+    @costo_total.setter
+    def costo_fijo(self,valor1):
+        self.__costo_fijo = valor1
+
+    def costo_variable (costo_total, costo_variable):
+        resultado = self.__costo_total - self.__costo_fijo
